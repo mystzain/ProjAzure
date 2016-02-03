b8e77319ab6e938051b5bba39a98e286c82168d7
mystz <msytzain@live.cn>
2016-02-03 20:56:14 +0800

    create

---
+++ Azure2/gulpfile.js
@@ -0,0 +1,12 @@
+﻿/// <binding Clean='clean' />
+var gulp = require('gulp');
+
+gulp.task('clean', function () {
+    // 将你的默认的任务代码放在这
+    gulp.src('./node_modules/angular/*')
+        .pipe(gulp.dest('./libs/angular'));
+    gulp.src('./node_modules/jquery/dist/*')
+        .pipe(gulp.dest('./libs/jquery'));
+    gulp.src('./node_modules/ui-router/release/*')
+        .pipe(gulp.dest('./libs/angular'));
+});
